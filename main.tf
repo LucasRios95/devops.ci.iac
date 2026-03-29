d31f15dc21f18ef1dad9eea18ce13195cb09d800
LucasRios95 <lucashrios95@gmail.com>
2026-03-29 10:00:23 -0300

    new: configure backend iac

--- main.tf
+++ main.tf
@@ -5,8 +5,34 @@ terraform {
       version = "6.34.0"
     }
   }
+  backend "s3" {
+    bucket = "devops-iac-ci"
+    key    = "state/terraform.tfstate"
+    region = "us-east-2"
+  }
+  
 }
 
 provider "aws" {
   region = "us-east-2"
 }
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "devops-iac-ci"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "devops-iac-ci"
+  versioning_configuration {
+    status = "Enabled"
+  }
+} 
